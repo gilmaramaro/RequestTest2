1f6311d71174ce617c405975beb22bcbb3377909
Gilmar Amaro <gilmar.amaro13@gmail.com>
2023-03-17 15:26:17 -0300

    Created Func Request

--- RequestTest2/RequestTest2/ViewController.swift
+++ RequestTest2/RequestTest2/ViewController.swift
@@ -6,14 +6,21 @@
 //
 
 import UIKit
-
+import Alamofire
 class ViewController: UIViewController {
 
+    var arrayPizza: [PizzaElement]?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    func setupRequest() {
+        AF.request("https://p3teufi0k9.execute-api.us-east-1.amazonaws.com/v1/pizza", method: .get).response { response in
+            let pizza = try? JSONDecoder().decode(Pizza.self, from: response.data ?? Data())
+            self.arrayPizza = pizza
+        }
+    }
 }
 
