1566c08298c10ff9aa0961e6766f92b4d3de7912
Gilmar Amaro <gilmar.amaro13@gmail.com>
2023-03-17 14:56:44 -0300

    Created Project

---
+++ RequestTest2/RequestTest2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RequestTest2
+//
+//  Created by Gilmar Amaro on 17/03/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
